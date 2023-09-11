818981436321ac66174afed0944b3129a4d33ab0
tubagusdylan <tubagus.dylanr@gmail.com>
2023-09-11 12:33:16 +0700

    create reference for mqtt topic

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -23,10 +23,18 @@ const Settings = (props) => {
           <h1 className="text-xl lg:text-2xl">Setting MQTT</h1>
         </header>
 
-        <section id="subcribe-topic" className="flex flex-wrap w-full gap-6 lg:gap-0">
+        <section id="subcribe-topic" className="flex flex-wrap w-full gap-6 lg:gap-0 mb-8">
           {setMqtt.map((value, index) => {
             return <SettingCard key={index} name={value.name} icon={value.icon} subcribeTopic={subcribeTopic} publishTopic={publishTopic} onChangeSubcribeTopic={onChangeSubcribeTopic} onChangePublishTopic={onChangePublishTopic} />;
           })}
+        </section>
+
+        <section id="info">
+          <h1 className="text-xl lg:text-2xl mb-6">Reference</h1>
+          <p className="text-lg">For further explanation or guidance in the writing mqtt topics, please visit the link below:</p>
+          <a href="https://www.hivemq.com/blog/mqtt-essentials-part-5-mqtt-topics-best-practices/" target="_blank" rel="noreferrer" className="underline underline-offset-4 text-teal-500 hover:text-teal-600 font-semibold text-lg">
+            MQTT Topics Best Practices
+          </a>
         </section>
       </div>
     </div>
